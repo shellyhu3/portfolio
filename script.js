d9703c75353b6eda37a7ce1167259ecded3f8afb
Shelly Hu <shellyhu3@gmail.com>
2019-10-29 16:13:30 -0700

    honeycomb navigation

--- script.js
+++ script.js
@@ -22,17 +22,19 @@ const isElementInViewport = (element) => {
   var scrollTop = $(window).scrollTop(),
     elementOffset = $(element).offset().top,
     distance = (elementOffset - scrollTop);
-  console.log(distance, $(window).height())
-  console.log($(window).height()*(4/5) > distance)
+  // console.log(distance, $(window).height())
+  // console.log($(window).height()*(4/5) > distance)
   return ($(window).height()*(4/5) > distance)
 }
 
 const checkAnimation = () => {
-  const elem1 = $('.title1')
-  const elem2 = $('.title2')
-  if (elem1.hasClass('animate') && elem2.hasClass('animate')) return;
+  const elem1 = $('.title1');
+  const elem2 = $('.title2');
+  const elem3 = $('.hexagon');
+  if (elem1.hasClass('animate') && elem2.hasClass('animate') && elem3.hasClass('fade_animation')) return;
   if (isElementInViewport(elem1)) {
     elem1.addClass('animate');
+    elem3.addClass('fade_animation');
   }
   if (isElementInViewport(elem2)) {
     elem2.addClass('animate');
@@ -43,3 +45,18 @@ $(document).on('scroll', () => {
   checkAnimation();
 })
 
+//honeycomb navigation animation
+let i = 0;
+const expand = () => {
+  if (i===0){
+    $('#nav_icon').html('<i class="fa fa-times fa-2x x"></i>');
+    $('.item').eq(0).css('transform','translate(64px, 37px)');
+    $('.item').eq(1).css('transform','translate(0px, 75px)');
+    i=1;
+  } else{
+    $('#nav_icon').html('<i class="fas fa-bars fa-2x bars"></i>');
+    $('.item').eq(0).css('transform','translateX(0px)');
+    $('.item').eq(1).css('transform','translateY(0px)');
+    i=0;
+  }
+}
